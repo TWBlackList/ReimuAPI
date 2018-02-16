2849ef9b28ca6142bc9953662b06f6d9a399289f
OwO <OwO@OmO.tw>
2018-02-16 14:17:46 +0800

    [config] fix name

--- ReimuBase/ConfigManager.cs
+++ ReimuBase/ConfigManager.cs
@@ -59,7 +59,7 @@ namespace ReimuAPI.ReimuBase
         public long admin_group { get; set; } = 0;
         public int[] admin_list { get; set; }
         public int[] sysop_list { get; set; }
-        public int[] white_list { get; set; }
+        public int[] whitelist { get; set; }
         public int[] blockgroup_list { get; set; }
         public PluginsListBundle plugins { get; set; }
     }
